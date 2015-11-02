f07fce56885682799a4fd2a9a811e4f000db41a4
fabrice aissah <faissah@jahia.com>
2015-11-02 15:06:28 -0500

    finished prototype implementation

--- src/main/java/org/jahia/modules/weatherprovider/WeatherDataSource.java
+++ src/main/java/org/jahia/modules/weatherprovider/WeatherDataSource.java
@@ -24,7 +24,7 @@ public class WeatherDataSource implements ExternalDataSource, ExternalDataSource
 
     public static final HashSet<String> ROOT_NODES = Sets.newHashSet("AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY");
 
-    private static String API_URL = "api.wunderground.com/api/";
+    private static String API_URL = "api.wunderground.com";
     private static String API_CONDITIONS = "/conditions/q/";
     private static String API_KEY = "api_key";
     private String apiKeyValue;
@@ -49,27 +49,106 @@ public class WeatherDataSource implements ExternalDataSource, ExternalDataSource
         List<String> r = new ArrayList<String>();
 
         String[] splitPath = path.split("/");
-        if (splitPath.length == 0) {
-            r.addAll(ROOT_NODES);
-            return r;
-        } else{
-            return Collections.emptyList();
+            try {
+                    if (splitPath.length == 0) {
+                        r.addAll(ROOT_NODES);
+                        return r;
+                    } else{
+                        switch (splitPath.length){
+                            case 2:
+                                JSONObject o;
+                                o = queryWeather("/api/"+apiKeyValue +"/conditions/q/"+ splitPath[1]+".json");
+                                JSONObject response = o.getJSONObject("response");
+                                JSONArray result = response.getJSONArray("results");
+
+                                //for (int i = 0; i < result.length(); i++) {
+                                //Limited the number of city per state at 3 since my dev key is pretty limited
+                                for (int i = 0; i < 3; i++) {
+                                        JSONObject city = result.getJSONObject(i);
+                                    r.add(city.getString("name"));
+                                }
+                                return r;
+                            case 3:
+                                return r;
+
+                        }
+                    }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public ExternalData getItemByIdentifier(String identifier) throws ItemNotFoundException {
+        String[] splitPath;
+        try {
+        if (identifier.equals("root")) {
+            return new ExternalData(identifier, "/", "jnt:contentFolder", new HashMap<String, String[]>());
+        }
+        if (identifier.contains("state-")) {
+            final String s = StringUtils.substringAfter(identifier, "state-");
+            if (ROOT_NODES.contains(s)) {
+                return new ExternalData(identifier, "/" + s, "jnt:contentFolder", new HashMap<String, String[]>());
+            }
+        }else if (identifier.contains("city-")) {
+            final String citykey = StringUtils.substringAfter(identifier, "city-");
+            splitPath = citykey.split("_");
+            JSONObject response;
+            response =  queryWeather("/api/" + apiKeyValue + "/conditions/q/" + splitPath[0] +"/"+splitPath[1] +".json");
+            JSONObject city = response.getJSONObject("current_observation").getJSONObject("display_location");
+            JSONObject city_observation = response.getJSONObject("current_observation");
+            Map<String, String[]> properties = new HashMap<String, String[]>();
+            if (city.getString("city") != null)
+                properties.put("city", new String[]{city.getString("city")});
+            if (city.getString("state") != null)
+                properties.put("state", new String[]{city.getString("state")});
+            if (city.getString("country") != null)
+                properties.put("country", new String[]{city.getString("country")});
+            if (city_observation.getString("temperature_string") != null)
+                properties.put("temperature_string", new String[]{city_observation.getString("temperature_string")});
+            if (city_observation.getString("feelslike_string") != null)
+                properties.put("feelslike_string", new String[]{city_observation.getString("feelslike_string")});
+            if (city_observation.getString("wind_string") != null)
+                properties.put("wind_string", new String[]{city_observation.getString("wind_string")});
+            if (city_observation.getString("precip_today_string") != null)
+                properties.put("precip_today_string", new String[]{city_observation.getString("precip_today_string")});
+            if (city_observation.getString("relative_humidity") != null)
+                properties.put("relative_humidity", new String[]{city_observation.getString("relative_humidity")});
+            return new ExternalData(identifier, getPathForCity(city), "jnt:city", properties);
+        }
+        return null;
+        } catch (Exception e) {
+            throw new ItemNotFoundException(e);
         }
     }
 
     @Override
-    public ExternalData getItemByIdentifier(String s) throws ItemNotFoundException {
-        return null;
-    }
-
-    @Override
-    public ExternalData getItemByPath(String s) throws PathNotFoundException {
+    public ExternalData getItemByPath(String path) throws PathNotFoundException {
+        String[] splitPath = path.split("/");
+        try {
+            if (path.endsWith("j:acl")) {
+                throw new PathNotFoundException(path);
+            }
+            if (splitPath.length <= 1) {
+                    return getItemByIdentifier("root");
+            }else{
+                switch (splitPath.length) {
+                    case 2:
+                        return getItemByIdentifier("state-"+ splitPath[1]);
+                    case 3:
+                        return getItemByIdentifier("city-" + splitPath[1]+"_"+ splitPath[2]);
+                }
+            }
+        } catch (ItemNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Set<String> getSupportedNodeTypes() {
-        return null;
+        return Sets.newHashSet("jnt:contentFolder", "jnt:state", "jnt:city");
     }
 
     @Override
@@ -106,26 +185,30 @@ public class WeatherDataSource implements ExternalDataSource, ExternalDataSource
     public List<String> search(ExternalQuery externalQuery) throws RepositoryException {
         List<String> results = new ArrayList<String>();
         String nodeType = QueryHelper.getNodeType(externalQuery.getSource());
-        JSONArray tmdbResult = null;
-        String year;
-        String month;
-
-        Map<String, Value> m = QueryHelper.getSimpleOrConstraints(externalQuery.getConstraint());
-        if (m.containsKey("jcr:title")) {
-            try {
-                tmdbResult = queryWeather("query", m.get("jcr:title").getString()).getJSONArray("results");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (tmdbResult != null) {
-            for (int i = 0; i < tmdbResult.length(); i++) {
-                final String path = "test";
-                if (path != null) {
-                    results.add(path);
+        JSONObject o = null;
+        JSONArray result= null;
+        try {
+            Map<String, Value> m = QueryHelper.getSimpleOrConstraints(externalQuery.getConstraint());
+            if (m.containsKey("n")) {
+                try {
+                    o = queryWeather("/api/"+apiKeyValue +"/conditions/q/US/"+ m.get("n").getString()+".json").getJSONObject("response");
+                    result = o.getJSONArray("results");
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
+
+            if (result != null) {
+                for (int i = 0; i < result.length(); i++) {
+                    final String path;
+                        path = getPathForCity(result.getJSONObject(i));
+                    if (path != null) {
+                        results.add(path);
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
         return results;
     }
@@ -145,20 +228,19 @@ public class WeatherDataSource implements ExternalDataSource, ExternalDataSource
 
     private JSONObject queryWeather(String path, String... params) throws RepositoryException {
         try {
-            HttpURL url = new HttpURL(API_URL, 80, path);
+            HttpURL url = new HttpURL(API_URL, -1, path);
 
             Map<String, String> m = new LinkedHashMap<String, String>();
             for (int i = 0; i < params.length; i += 2) {
                 m.put(params[i], params[i + 1]);
             }
-            m.put(API_KEY, apiKeyValue);
 
             url.setQuery(m.keySet().toArray(new String[m.size()]), m.values().toArray(new String[m.size()]));
             long l = System.currentTimeMillis();
             System.out.println("Start request : " + url);
             GetMethod httpMethod = new GetMethod(url.toString());
             try {
-                httpClient.getParams().setSoTimeout(10000);
+                httpClient.getParams().setSoTimeout(15000);
                 httpClient.executeMethod(httpMethod);
                 return new JSONObject(httpMethod.getResponseBodyAsString());
             } finally {
@@ -168,6 +250,10 @@ public class WeatherDataSource implements ExternalDataSource, ExternalDataSource
         } catch (Exception e) {
             throw new RepositoryException(e);
         }
+    }
+
+    private String getPathForCity(JSONObject city) throws JSONException {
+        return "/" + StringUtils.substringBeforeLast(city.getString("state"), "-").replace("-", "/") + "/" + city.getString("city");
     }
 
     public void setApiKeyValue(String apiKeyValue) {
